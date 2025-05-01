9473604276b9e650b7235dd4a1edbf5b411168ba
mateo <mateoq152006@gmail.com>
2025-05-01 17:26:56 -0500

    se hace una correccion de las clases padres

--- TrabajoComeeGalletas/src/autonoma/gamebase/elements/GraphicContainer.java
+++ TrabajoComeeGalletas/src/autonoma/gamebase/elements/GraphicContainer.java
@@ -1,17 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package autonoma.gamebase.elements;
 
 import java.awt.Rectangle;
 
 /**
- *
- * @author educacion
+ * Interfaz que define los metodos necesarios para un contenedor grafico.
+ * Cualquier clase que implemente esta interfaz debe poder actualizarse
+ * y proporcionar sus limites (boundaries) como un objeto Rectangle.
  */
 public interface GraphicContainer {
+
+    /**
+     * Metodo para actualizar o refrescar el contenido grafico del contenedor.
+     */
     public void refresh();
+
+    /**
+     * Metodo para obtener los limites del contenedor grafico.
+     * @return Rectangle que representa los limites del contenedor
+     */
     public Rectangle getBoundaries();
 }
--- TrabajoComeeGalletas/src/autonoma/gamebase/elements/Sprite.java
+++ TrabajoComeeGalletas/src/autonoma/gamebase/elements/Sprite.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package autonoma.gamebase.elements;
 
 import java.awt.Color;
@@ -11,11 +6,10 @@ import java.awt.Rectangle;
 import javax.swing.ImageIcon;
 
 /**
- *
- * @author educacion
+ * Clase abstracta que representa un objeto grafico (sprite) en el juego.
+ * Un sprite tiene posicion, dimensiones, color, imagen y esta contenido en un GraphicContainer.
  */
-public abstract class Sprite 
-{
+public abstract class Sprite {
     protected int x;
     protected int y;
     protected int height;
@@ -24,95 +18,84 @@ public abstract class Sprite
     protected ImageIcon image;
     protected GraphicContainer gameContainer;
 
+    /**
+     * Constructor del sprite con posicion y tamaño iniciales.
+     * @param x coordenada en X
+     * @param y coordenada en Y
+     * @param height alto del sprite
+     * @param width ancho del sprite
+     */
     public Sprite(int x, int y, int height, int width) {
         this.x = x;
         this.y = y;
         this.height = height;
         this.width = width;
     }
-    
-    public boolean isOutOfGraphicContainer()
-    {
+
+    /**
+     * Verifica si el sprite esta fuera del contenedor grafico.
+     * @return true si esta fuera, false si esta dentro
+     */
+    public boolean isOutOfGraphicContainer() {
         return isOutOfGraphicContainer(x, y, width, height);
     }
 
-    public boolean isOutOfGraphicContainer(int x, int y, int width, int height)
-    {
-        if(gameContainer == null)
+    /**
+     * Verifica si un rectangulo con las coordenadas dadas esta fuera del contenedor grafico.
+     * @return true si esta fuera, false si esta dentro
+     */
+    public boolean isOutOfGraphicContainer(int x, int y, int width, int height) {
+        if (gameContainer == null)
             return false;
-        
+
         Rectangle bounds = gameContainer.getBoundaries();
-        
-        return !(x >= bounds.getX() &
-                 y >= bounds.getY() &
-                 x + width  <= bounds.getX() + bounds.getWidth() &
+
+        return !(x >= bounds.getX() &&
+                 y >= bounds.getY() &&
+                 x + width <= bounds.getX() + bounds.getWidth() &&
                  y + height <= bounds.getY() + bounds.getHeight());
     }
-    
-    public boolean checkCollision(Sprite other)
-    {
-        // Collision x-axis?
+
+    /**
+     * Verifica si este sprite colisiona con otro sprite.
+     * @param other otro sprite
+     * @return true si hay colision, false si no
+     */
+    public boolean checkCollision(Sprite other) {
         boolean collisionX = this.getX() + this.getWidth() >= other.getX() &&
-            this.getX() < other.getX() + other.getWidth();
+                             this.getX() < other.getX() + other.getWidth();
 
-        // Collision y-axis?
         boolean collisionY = this.getY() + this.getHeight() >= other.getY() &&
-            this.getY() < other.getY() + other.getHeight();
+                             this.getY() < other.getY() + other.getHeight();
 
-        // Collision only if on both axes
-        return collisionX && collisionY;        
+        return collisionX && collisionY;
     }
 
+    /**
+     * Metodo abstracto que cada sprite debe implementar para pintarse.
+     * @param g el objeto Graphics donde se dibujara
+     */
     public abstract void paint(Graphics g);
-    
-    public int getX() {
-        return x;
-    }
 
-    public void setX(int x) {
-        this.x = x;
-    }
+    // Metodos getters y setters
+    public int getX() { return x; }
+    public void setX(int x) { this.x = x; }
 
-    public int getY() {
-        return y;
-    }
+    public int getY() { return y; }
+    public void setY(int y) { this.y = y; }
 
-    public void setY(int y) {
-        this.y = y;
-    }
+    public int getHeight() { return height; }
+    public void setHeight(int height) { this.height = height; }
 
-    public int getHeight() {
-        return height;
-    }
+    public int getWidth() { return width; }
+    public void setWidth(int width) { this.width = width; }
 
-    public void setHeight(int height) {
-        this.height = height;
-    }
+    public Color getColor() { return color; }
+    public void setColor(Color color) { this.color = color; }
 
-    public int getWidth() {
-        return width;
-    }
+    public ImageIcon getImage() { return image; }
+    public void setImage(ImageIcon image) { this.image = image; }
 
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
-    }
-
-    public ImageIcon getImage() {
-        return image;
-    }
-
-    public void setImage(ImageIcon image) {
-        this.image = image;
-    }
-    
     public void setGraphicContainer(GraphicContainer gContainer) {
         this.gameContainer = gContainer;
     }
